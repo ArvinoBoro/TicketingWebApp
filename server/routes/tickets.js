681a7fd15a4ea0f61bfe9fef34c88f528ded752f
ArvinoBoro <arvin.ziba@ontariotechu.net>
2024-11-09 15:29:09 -0500

    Initialize basic file structure

---
+++ server/routes/tickets.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let router = express.Router(); 
+
+/* Read Functionality - Dev */ 
+
+/* Update Functionality - Arvin */
+
+/* Create Functionality - Carson */
+
+/* Delete Functionality - Carson */ 
+
+module.exports = router;
