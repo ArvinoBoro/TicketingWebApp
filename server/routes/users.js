9c4d049c37326a1e2e668d39f9d7b01c679cf5c7
Devisveryfunny <dev.parmar@ontariotechu.net>
2024-11-28 14:13:41 -0500

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,21 @@
 let express = require('express');
 let router = express.Router();
+let mongoose = require('mongoose'); 
+let User = require('../models/user_model');
 let passport = require('passport');
-let User = require('../models/user_model');  // Assuming the user model is in models/user_model.js
 
-// GET Login Page
 router.get('/login', (req, res, next) => {
-  res.render('auth/login', { title: 'Login' });
+  if(!req.user) {
+    res.render('auth/login', 
+      {
+        title: 'Login',
+        message: req.flash('loginMessage'),
+        displayName: req.user ? req.user.displayName:''
+        })
+  } 
+  else {
+    return res.redirect('/')
+  }
 });
 
 // POST Login (Authenticate)
@@ -15,13 +25,15 @@ router.post('/login', (req, res, next) => {
       return next(err);  // Handle errors
     }
     if (!user) {
-      return res.redirect('/users/login');  // Redirect if login fails
+      req.flash('loginMessage',
+        'AuthenticationError');
+         return res.redirect('/users/login');  // Redirect if login fails
     }
     req.login(user, (err) => {
       if (err) {
         return next(err);
       }
-      return res.redirect('/');  // Redirect to home or dashboard after successful login
+      return res.redirect('/tickets');  // Redirect to home or dashboard after successful login
     });
   })(req, res, next);
 });
@@ -35,5 +47,44 @@ router.get('/logout', (req, res) => {
     res.redirect('/');  // Redirect to home page after logout
   });
 });
+
+
+router.get('/register', (req, res, next) => {
+  if(!req.user) {
+    res.render('auth/register', {
+        title: 'Register',
+        message: req.flash('registerMessage'),
+        displayName: req.user ? req.user.displayName:''
+        });
+  } else {
+    return res.redirect('/');
+  }
+});
+
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    username: req.body.username,
+    email:req.body.email,
+    displayName:req.body.displayName
+  });
+  User.register(newUser, req.body.password, (err) => {
+    if(err) {
+      console.log("Error: Inserting the new user");
+      if(err.name == "UserExistsError") {
+          req.flash('registerMessage', 'Registration Error: User already exists');
+      }
+      return res.render('auth/register', {
+          title:'Register',
+          message:req.flash('registerMessage'),
+          displayName: req.user ? req.user.displayName:''
+        });
+    } else {
+      return passport.authenticate('local')(req, res, () => {
+        res.redirect('/tickets')
+      });
+    }
+  });
+});
+
 
 module.exports = router;
