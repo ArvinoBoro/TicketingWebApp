f84ef4cfd67c15feb302df687e64ff6ecf30a9e9
Arvin <144701407+ArvinoBoro@users.noreply.github.com>
2024-12-04 11:55:42 -0500

    Fix callback URL

--- server/config/app.js
+++ server/config/app.js
@@ -55,7 +55,7 @@ passport.deserializeUser(User.deserializeUser());
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.GITHUB_CALLBACK_URI 
+  callbackURL: process.env.GITHUB_CALLBACK_URL
 }, async function(accessToken, refreshToken, profile, done) {
   try {
     
