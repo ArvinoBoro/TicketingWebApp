896c15b07d4a4d6b1c45eee1f5895bbb638073b0
CarsonBlackOTU <carson.black@ontariotechu.net>
2024-12-04 20:03:15 -0500

    angular navbar works

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,17 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AppComponent } from './app.component';
+import { AddComponent } from './pages/add/add.component';
+import { ListComponent } from './pages/list/list.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: AppComponent },
+    {path:'', redirectTo: 'home', pathMatch: 'full' },  // Redirect to home page if no path is provided
+    {path:'home', component: HomeComponent },
+    {path:'list', component: ListComponent},
+    {path:'add', component: AddComponent},
+    {path:'login', component: LoginComponent},
+];
