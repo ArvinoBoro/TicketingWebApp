e99fcdb9c85b97e773bafaae3ccd19bbe9af970c
CarsonBlackOTU <carson.black@ontariotechu.net>
2024-12-04 22:35:52 -0500

    updated routes

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -6,12 +6,17 @@ import { AddComponent } from './pages/add/add.component';
 import { ListComponent } from './pages/list/list.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { EditComponent } from './pages/edit/edit.component';
+import { DeleteComponent } from './pages/delete/delete.component';
 
 export const routes: Routes = [
     {path:'', component: AppComponent },
     {path:'', redirectTo: 'home', pathMatch: 'full' },  // Redirect to home page if no path is provided
     {path:'home', component: HomeComponent },
-    {path:'list', component: ListComponent},
-    {path:'add', component: AddComponent},
-    {path:'login', component: LoginComponent},
+    {path:'tickets', component: ListComponent},
+    {path:'tickets/add', component: AddComponent},
+    {path:'tickets/edit/:id', component: EditComponent},
+    {path:'tickets/delete/:id', component: DeleteComponent},  // Redirect to add page for delete operation
+    {path:'users/login', component: LoginComponent},
+    {path:'users/register', component: RegisterComponent},
 ];
